3674250f8c536a579414744620214dc5a5739024
Caique Ortega <cortegamdx@gmail.com>
2021-05-04 15:52:59 -0300

    Static charts

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const DataTable = () => {
   return (
-    <div className="table-responsive">
+    <div className="table-responsive mb-4">
       <table className="table table-striped table-sm">
         <thead>
           <tr>
@@ -15,6 +15,90 @@ const DataTable = () => {
           </tr>
         </thead>
         <tbody>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
           <tr>
             <td>22/04/2021</td>
             <td>Barry Allen</td>
