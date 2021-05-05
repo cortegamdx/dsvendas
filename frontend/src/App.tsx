07aa89c4c769e08dd8e1f6820172bc40fc471098
Caique Ortega <cortegamdx@gmail.com>
2021-05-05 08:35:40 -0300

    Security config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
 import Footer from 'components/Footer';
 import NavBar from 'components/NavBar';
 import DataTable from 'components/DataTable';
 import BarChart from 'components/BarChart';
 import DonutChart from 'components/DonutChart';
+
+
 const App = () => {
   return (
     <>
